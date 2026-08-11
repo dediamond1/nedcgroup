07608895e278325d0cd4198a8d94bb5072d20b25
root <root@1770833391735-74a6d87a7787.local>
2026-08-12 01:39:02 +0200

    Point API base at production: https://nedcgroup.techdevcyber.se (replaces dev http://64.112.124.208:3100)

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,6 +3,6 @@
 // export const baseUrl = 'https://artinsapi.techdevcyber.se';
 // PROD (Netlify): 'https://chic-melomakarona-4b346e.netlify.app'
 // DEV (Next.js dev server):
-export const baseUrl = 'http://64.112.124.208:3100';
+export const baseUrl = 'https://nedcgroup.techdevcyber.se';
 // export const baseUrl = 'http://192.168.1.96:3004';
 
